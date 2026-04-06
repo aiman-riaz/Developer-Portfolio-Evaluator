fff6d21b01353f00209767da8c29e0ef848263d3
Aiman Ahmed Riaz <aimanahmed655@gmail.com>
2026-04-06 17:13:26 +0530

    Fixed the heatmap dropping contributions by switching to the GraphQL calendar

--- server/services/githubService.js
+++ server/services/githubService.js
@@ -1,7 +1,9 @@
 const { Octokit } = require('@octokit/rest');
+
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
+
 const getUser = async (username) => {
   try {
     const { data } = await octokit.rest.users.getByUsername({ username });
@@ -15,6 +17,7 @@ const getUser = async (username) => {
     throw err;
   }
 };
+
 const getRepos = async (username) => {
   const { data } = await octokit.rest.repos.listForUser({
     username,
@@ -23,27 +26,53 @@ const getRepos = async (username) => {
   });
   return data;
 };
+
 const getEvents = async (username) => {
   try {
-    const { data } = await octokit.rest.activity.listPublicEventsForUser({
-      username,
-      per_page: 100,
-    });
-    return data;
+    const pages = await Promise.all([
+      octokit.rest.activity.listPublicEventsForUser({ username, per_page: 100, page: 1 }),
+      octokit.rest.activity.listPublicEventsForUser({ username, per_page: 100, page: 2 }),
+      octokit.rest.activity.listPublicEventsForUser({ username, per_page: 100, page: 3 }),
+    ]);
+    return pages.flatMap((p) => p.data);
   } catch {
     return [];
   }
 };
+
+const getContributions = async (username) => {
+  try {
+    const query = `
+      query($userName:String!) {
+        user(login: $userName){
+          contributionsCollection {
+            contributionCalendar {
+              totalContributions
+              weeks {
+                contributionDays {
+                  contributionCount
+                  date
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+    const result = await octokit.graphql(query, { userName: username });
+    return result.user.contributionsCollection.contributionCalendar;
+  } catch (err) {
+    return null;
+  }
+};
+
 const getRepoContents = async (owner, repo) => {
   try {
-    const { data } = await octokit.rest.repos.getContent({
-      owner,
-      repo,
-      path: '',
-    });
+    const { data } = await octokit.rest.repos.getContent({ owner, repo, path: '' });
     return Array.isArray(data) ? data : [];
   } catch {
     return [];
   }
 };
-module.exports = { getUser, getRepos, getEvents, getRepoContents };
+
+module.exports = { getUser, getRepos, getEvents, getRepoContents, getContributions };
