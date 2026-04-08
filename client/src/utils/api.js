87583cdf4ac068be7549b0f66a695ad807452567
Aiman Ahmed Riaz <aimanahmed655@gmail.com>
2026-04-08 17:47:18 +0530

    updated api details to avoid errors

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,9 +3,9 @@ const BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 const api = axios.create({ baseURL: BASE });
 export const fetchProfile = async (username) => {
   const { data } = await api.get(`/profile/${username}`);
-  return data; // { cached: bool, data: reportObj }
+  return data;
 };
 export const fetchCompare = async (u1, u2) => {
   const { data } = await api.get(`/compare?u1=${u1}&u2=${u2}`);
-  return data; // { data: [report1, report2] }
+  return data;
 };
